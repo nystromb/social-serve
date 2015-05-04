4e53fb75306174810e2e260c73edac96165a22b3
Brian Nystrom <bnystrom@siu.edu>
2015-05-04 16:00:41 -0500

    Revert "Add dinner party form changes & small revisions"
    This reverts commit af862d4d466053b6a16cc88f17f5166aeb90bf00.

--- app/src/main/java/app/com/socialserve/AddDinnerParty.java
+++ app/src/main/java/app/com/socialserve/AddDinnerParty.java
@@ -2,15 +2,23 @@ package app.com.socialserve;
 
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.parse.Parse;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class AddDinnerParty extends ActionBarActivity {
     EditText eventName, eventAddress, seatsAvail, eventDate, eventDescription, ingredients;
