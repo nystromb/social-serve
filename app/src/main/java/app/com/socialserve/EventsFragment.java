dcf355aea1183a1190c5c66e75a1c3e2ffafa747
Brian Nystrom <bnystrom@siu.edu>
2015-05-06 14:10:17 -0500

    Comment cleanup

--- app/src/main/java/app/com/socialserve/EventsFragment.java
+++ app/src/main/java/app/com/socialserve/EventsFragment.java
@@ -7,10 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-
 import com.parse.ParseQueryAdapter;
 import com.parse.ParseUser;
 
@@ -25,25 +22,15 @@ public class EventsFragment extends Fragment {
     private ParseQueryAdapter mainAdapter;
     private DinnerPartyAdapter eventsAdapter;
 
-
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-
-        // This makes sure that the container activity has implemented
-        // the callback interface. If not, it throws an exception
-     /*   try {
-            listener = (RegisterOnClickListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement LoginInterfaceListener");
-        }
-        */
     }
 
     public EventsFragment() {
         // Required empty public constructor
     }
+
     public static EventsFragment newInstance(int sectionNumber) {
         EventsFragment fragment = new EventsFragment();
         Bundle args = new Bundle();
@@ -58,10 +45,8 @@ public class EventsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_events, container, false);
+        //set up the adapter and apply to the ListView
         mainAdapter = new DinnerPartyAdapter(view.getContext(), "host", ParseUser.getCurrentUser().getEmail());
-
-        //Subclass of Parse Query Adapter
-
         eventsList = (ListView) view.findViewById(R.id.eventsListView);
         eventsList.setAdapter(mainAdapter);
         return view;
